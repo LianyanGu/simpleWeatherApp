68afe2afd50093d056c3fbcdf2e0866d9dde8c6b
Gu, Lianyan (CORP) <Lianyan.Gu@adp.com>
2018-08-21 14:34:07 -0400

    Update simple NodeJs project

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 let request = require('request');
+const argv = require('yargs').argv;
 
-let apiKey = '';
-let city = 'Alpharetta';
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+let apiKey = '3d313d4e9b6031474b70207db742a6f4';
+let city = argv.v || 'Alpharetta';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
 
 request(url, function (err, response, body) {
     if (err) {
         console.log('error:', error);
     } else {
-        console.log('body:', body);
+        let weather = JSON.parse(body)
+        let message = `It's ${weather.main.temp} degrees in ${weather.name}`;
+        console.log(weather);
+        console.log(message)
     }
 });
